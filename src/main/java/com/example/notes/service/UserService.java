c7f08e7ba3c353a0fa9af33d680c4da2c6aa8c04
Venkatesh K <venkat@finzy.com>
2020-07-23 01:14:41 +0530

    fixed password

--- src/main/java/com/example/notes/service/UserService.java
+++ src/main/java/com/example/notes/service/UserService.java
@@ -9,6 +9,7 @@ public class UserService {
 
     public Login login(User user) {
         if(user.getId().equals("user1") || user.getId().equals("user2")) {
+            if(user.getPasswordHash().equals("password"))
             return new Login(
                     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1c2VyMSIsIm5hbWUiOiJ1c2VyMSIsImlhdCI6MTUxNjIzOTAyMn0.8LX-Ti-KyYJ3A4m22Nt7vYUGMrh4ymjJQ8THMSytBc8",
                     "success");
